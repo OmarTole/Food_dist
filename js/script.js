75fe48f09da05a3247e7a8bb73e4e472773a0c60
Tole Bi <omartole23@gmail.com>
2023-08-19 20:32:38 +0600

    npm and JSON server

--- js/script.js
+++ js/script.js
@@ -305,5 +305,9 @@ document.addEventListener('DOMContentLoaded', () => {
             modalContent.style.display = 'block';
         }, 4000)
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
     
 });
