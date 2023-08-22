b17b3536d9b9f9b59b2c9be8074c01c3e22b33ac
Tole Bi <omartole23@gmail.com>
2023-08-22 21:13:15 +0600

    GET date with server and async/await

--- js/script.js
+++ js/script.js
@@ -136,7 +136,6 @@ document.addEventListener('DOMContentLoaded', () => {
     modal.addEventListener('keydown', (e) => {
         if (e.code === 'Escape' && modal.style.contains('block: none')) {
             closeModal();
-            console.log('Escape');
         }
     });
 
@@ -170,7 +169,7 @@ document.addEventListener('DOMContentLoaded', () => {
             this.price = this.price * this.transfer; 
         }
 
-        myMenu() {
+        render() {
             const element = document.createElement('div');
 
             if (this.classes.length === 0) {
@@ -193,52 +192,50 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих 
-        овощей и фруктов. Продукт активных и здоровых людей.
-        Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
-        10,
-        '.menu .container'
-        ).myMenu();
-        
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню "Премиум"',
-        `В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.
-         Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
-        5,
-        '.menu .container'
-        ).myMenu();
+    const getResource = async (url) => {
+        const res = await fetch(url)
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля,
-         овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.`,
-        7,
-        '.menu .container'
-        ).myMenu();
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json()
+    }
+
+    getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render()
+            })
+        });
 
     //Forms
      
     const forms = document.querySelectorAll('form');
 
     const message = {
-        loading: 'img/slider.svg',
+        loading: 'img/spinner.svg',
         success: 'Спасибо! Скоро мы с вами свяжемся',
         failure: 'Что-то пошло не так...',
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            body: data,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+
+        return await res.json()
+    }
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -253,19 +250,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key) {
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()))
 
-            fetch('server.php', {
-                method: 'POST',
-                body: JSON.stringify(object),
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
-            .then(data => data.text())
+            postData('http://localhost:3000/requests', json)
             .then(data => {
                 statusModal(message.success);
                 form.reset();
